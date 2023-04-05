fc8514a42109cfee465888b9407b1282715200d9
Abdou <2000abdallah2014@gmail.com>
2023-04-05 10:13:43 +0100

    created default lists + connected the store to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="global-container">
+    <div class="global-container flex flex-col justify-between">
         <nav class="flex p-6">
             <div class="mode-switch"> <v-btn icon="mdi-theme-light-dark"></v-btn></div>
             <div class='nav-btns-cont'>
@@ -7,49 +7,89 @@
                 <v-btn icon="mdi-cog"></v-btn>
             </div>
         </nav>
-        <div class="results-cont">
+        <section class="results-cont flex flex-col">
             <div class="operation-view">1+23+1</div>
             <p class="inputs-number">you have entered : 4 inputs</p>
             <p class="result">25</p>
-        </div>
-        <div class="btns-cont">
-            <div class="op-btn">One</div>
+        </section>
+        <section class="btns-cont">
+            <div class="op-btn">C</div>
             <div class="op-btn">Two</div>
-            <div>Three</div>
-            <div>Four</div>
-            <div>Five</div>
-            <div>One</div>
-            <div>Two</div>
-            <div>Three</div>
-            <div>Four</div>
-            <div>Five</div>
-            <div>Five</div>
-            <div>One</div>
-            <div>Two</div>
-            <div>Three</div>
-            <div>Four</div>
-            <div>Five</div>
-            <div>Four</div>
-            <div>Five</div>
-        </div>
+            <button v-for="num in lists[currrentList]">{{ num }}</button>
+        </section>
     </div>
 </template>
+<script setup>
+import { useSettingStore } from '~/store/settingStore';
+
+const { lists, currrentList } = useSettingStore();
+
+
+</script>
 <style>
+.global-container {
+    min-height: 100vh;
+    padding-bottom: 2rem;
+    background: #17171C;
+    color: white;
+}
+
 .mode-switch,
 .nav-btns-cont {
     margin-left: auto;
 }
 
+section {
+    margin-inline: 1.2rem;
+}
+
+/*section 1 */
+.operation-view {
+    padding-bottom: 7rem;
+    font-family: 'Work Sans';
+    font-style: normal;
+    font-size: 1.4rem;
+    opacity: 0.8;
+    font-family: 'Work Sans';
+}
+
+.inputs-number {
+    text-align: center;
+    font-size: 1.3rem;
+    opacity: 0.8;
+    font-family: 'Work Sans';
+}
+
+.result {
+    margin-left: auto;
+    font-family: 'Work Sans';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 2.6rem;
+}
+
+
+/*sction2 */
 .btns-cont {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 1rem;
+    grid-column-gap: 0.5rem;
 }
 
 .btns-cont>* {
-    text-align: center;
+    display: flex;
+    min-height: 3rem;
+    border-radius: 24px;
+    align-items: center;
+    justify-content: center;
+    background: #2E2F38;
+
 }
 
 .op-btn {
     grid-column: span 2;
+    background: rgba(252, 75, 75, 1);
+
 }
 </style>
---
+++ store/settingStore.js
@@ -0,0 +1,32 @@
+import { defineStore, acceptHMRUpdate } from "pinia";
+
+
+export const useSettingStore = defineStore("SettingStore", () => {
+const currrentList = ref("smallNumbersList");
+
+ 
+
+const lists = ref({
+smallNumbersList : [0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,3.25,3.5,3.75,4],
+oneStepList :  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+tenStepsList :  [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170],
+fiftyStepsList :  [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800],
+oneHundredSteps :  [100,200,300,400,500,600,700,800],
+ amercianMethodList: [ -4, -3, -2, -1,1, 2, 3, 4,]
+})
+
+
+
+  return {
+    currrentList, lists,
+  };
+},
+/* Enable this to persist this store : more info : https://prazdevs.github.io/pinia-plugin-persistedstate/frameworks/nuxt-3.html
+{
+  persist: true}
+*/
+);
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useSettingStore, import.meta.hot));
+}
